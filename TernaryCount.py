9bb506a1e38ee539257a7a08d0110250c2008434
rovvit <vitazzz96@gmail.com>
2022-11-02 14:38:19 +0600

    Increment from -1 (-1, 0, 1)

--- TernaryCount.py
+++ TernaryCount.py
@@ -1,14 +1,23 @@
 # Increment in ternary cycle
 num = 3                        # число разрядов
 trenary = []                    # список 0-2
+startPoint = 0
 for _ in range (num):
-    trenary.append(-1)
+    trenary.append(startPoint)
 n = 0
 while n < 3**num-1:
     print(trenary)
     trenary[0] += 1             # инкремент
-    for i in range(num):        # проверка на перенос единицы в следующий разряд
-        if trenary[i] > 1:
-            trenary[i] = -1
-            trenary[i+1] += 1
+    # for i in range(num):        # проверка на перенос единицы в следующий разряд
+    #     if trenary[i] > 1:
+    #         trenary[i] = -1
+    #         trenary[i+1] += 1
+    i = 0
+    while trenary[i] > 2 + startPoint:
+        trenary[i] = startPoint
+        trenary[i+1] += 1
+        i += 1
+
     n+=1
+
+
